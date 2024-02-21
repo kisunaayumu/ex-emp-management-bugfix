0261b1ec6680d02f14dd40f766c3c99c403cec58
user <ayumu.kisuna@rakus-partners.co.jp>
2024-02-21 16:48:51 +0900

    4-2

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,8 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
+	
+
 	/**
 	 * 使用するフォームオブジェクトをリクエストスコープに格納する.
 	 * 
@@ -49,10 +52,10 @@ public class EmployeeController {
 	 * @return 従業員一覧画面
 	 */
 	@GetMapping("/showList")
-	public String showList(Model model) {
-		List<Employee> employeeList = employeeService.showList();
-		model.addAttribute("employeeList", employeeList);
-		return "employee/list";
+	public String showList(Model model){
+	List<Employee> employeeList = employeeService.showList();
+	model.addAttribute("employeeList", employeeList);
+	return "employee/list";
 	}
 
 	/////////////////////////////////////////////////////
