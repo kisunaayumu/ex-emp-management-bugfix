89d16eeb070c1bc7308ca8464b5b74e9f4e49841
user <ayumu.kisuna@rakus-partners.co.jp>
2024-02-22 14:35:59 +0900

    6-5修正です

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -3,7 +3,6 @@ package com.example.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
